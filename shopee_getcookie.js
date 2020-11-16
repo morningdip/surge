b8cad510a083cc9c07dca8ed6f83a1dfe57d4e77
morningdip <kaka830313@gmail.com>
2020-11-16 16:21:41 +0800

    Update shopee_getcookie.js

--- shopee_getcookie.js
+++ shopee_getcookie.js
@@ -1,20 +1,13 @@
-/*
-Shoppe Check in Get Cookie.
-The following URL check in once
-https://shopee.tw
-http-request ^https:\/\/shopee\.tw\/me\/setting max-size=0,script-path=shopee_getcookie.js
-MITM = shopee.tw
-*/
-
 if ($request.headers['Cookie']) {
     var headerSP = $request.headers['Cookie'];
     var cookie = $persistentStore.write(headerSP, "CookieSP");
-    if (!cookie){
-      $notification.post("Shopee Cookie Error‼️", "", "Try again later")
+    if (!cookie) {
+      $notification.post("Shopee Cookie Error ‼️", "", "Try again later")
     } else {
-      $notification.post("Shopee Cookie Get🎉🎉", "", "")
+      $notification.post("Shopee Cookie Get 🎉🎉", "", "")
     }
-  } else {
-    $notification.post("Shopee Cookie Error‼️", "", "Try again later")
-  }
-  $done({})
+} else {
+    $notification.post("Shopee Cookie Error ‼️", "", "Try again later")
+}
+
+$done({})
