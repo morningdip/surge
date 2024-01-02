7c039a17cf1ce95f1dcf891a9a12109e670b344f
morningdip <kaka830313@gmail.com>
2024-01-02 11:31:51 +0800

    add: max length etc settings in cathay 3d verification

---
+++ scripts/cathay_3d.js
@@ -0,0 +1,5 @@
+let body = $response.body;
+// change type to number
+body = body.replace(/type="password"/g, 'type="number" maxlength="4" inputmode="numeric"');
+body = body.replace(/autocomplete="off"/g, 'autocomplete="one-time-code" autocorrect="off"');
+$done({ body });
