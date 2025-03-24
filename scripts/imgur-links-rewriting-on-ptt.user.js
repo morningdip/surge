7e092095fa363be4ae17bff02ece6f84d41c1ab8
morningdip <kaka830313@gmail.com>
2025-03-25 01:24:08 +0800

    rewrite: imgur-links-rewriting-on-ptt.user.js

--- scripts/imgur-links-rewriting-on-ptt.user.js
+++ scripts/imgur-links-rewriting-on-ptt.user.js
@@ -1,23 +1,8 @@
-const re_imgur_links = /<a[^>]*href="(https?:\/\/(?:i\.|m\.)?imgur\.com\/w+\.\w+)"[^>]*>/g;
+let body = $response.body;
 
-let modifiedBody = $response.body;
+body = body.replace(
+  "</body>",
+  '<script defer type="text/javascript" src="https://raw.githubusercontent.com/gslin/imgur-links-rewriting-on-ptt/refs/heads/master/imgur-links-rewriting-on-ptt.user.js"></script></body>',
+);
 
-// 使用正則表達式找到並替換 Imgur 的連結
-modifiedBody = modifiedBody.replace(re_imgur_links, (match, url) => {
- const imgurIdMatch = url.match(/https?:\/\/(?:i\.|m\.)?imgur\.com\/(\w+)\.?(\w+)?/);
-  if (imgurIdMatch) {
-    const imgurId = imgurIdMatch[1];
-    let imgurSuffix = imgurIdMatch[2] || 'webp';
-    if (imgurSuffix === 'gif') {
-      imgurSuffix = 'gif'; // 保留 gif
-    }
-    const imgTag = `<div style="margin-top:0.5em;text-align:center;"><img referrerpolicy="no-referrer" src="https://i.imgur.com/${imgurId}.${imgurSuffix}" /></div>`;
-    return match + imgTag; // 在原始連結後插入圖片
-  }
- return match;
-});
-
-// 返回修改後的 HTML
-$done({
-  body: modifiedBody
-});
+$done({ body });
