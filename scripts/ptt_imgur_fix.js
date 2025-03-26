53bf23b4ebcc5dfeeba0bdf7bba3a14cd19ee343
morningdip <kaka830313@gmail.com>
2025-03-26 10:25:06 +0800

    fix: change js src

--- scripts/ptt_imgur_fix.js
+++ scripts/ptt_imgur_fix.js
@@ -2,7 +2,7 @@ let body = $response.body;
 
 body = body.replace(
   "</body>",
-  '<script defer type="text/javascript" src="//kinta.ma/surge/scripts/imgur-links-rewriting-on-ptt.user.js"></script></body>',
+  '<script defer type="text/javascript" src="https://cdn.jsdelivr.net/gh/gslin/imgur-links-rewriting-on-ptt/imgur-links-rewriting-on-ptt.user.js"></script></body>',
 );
 
 $done({ body });
