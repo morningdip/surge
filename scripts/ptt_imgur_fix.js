9ef0ea96a743dce48bd7d2a5be10bb305804fbfa
morningdip <kaka830313@gmail.com>
2025-03-28 12:38:30 +0800

    fix: ptt_imgur_fix.js

--- scripts/ptt_imgur_fix.js
+++ scripts/ptt_imgur_fix.js
@@ -2,7 +2,7 @@ let body = $response.body;
 
 body = body.replace(
   "</body>",
-  '<script defer type="text/javascript" src="https://cdn.jsdelivr.net/gh/gslin/imgur-links-rewriting-on-ptt/imgur-links-rewriting-on-ptt.user.js"></script></body>',
+  '<script defer type="text/javascript" src="cdn.jsdelivr.net/gh/gslin/imgur-links-rewriting-on-ptt/imgur-links-rewriting-on-ptt.user.js"></script></body>',
 );
 
 $done({ body });
